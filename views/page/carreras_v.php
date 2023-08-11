6968a309c228e4d3395c675edfd2f9bfe3170654
Andres Aguilar <aaguilarc@uach.mx>
2023-08-11 13:47:44 -0600

    Dialog corregidor de rutas de aprendizaje

--- views/page/carreras_v.php
+++ views/page/carreras_v.php
@@ -1,7 +1,7 @@
 <div class="container mx-auto mt-5">
     <div class="flex justify-end">
         <button data-open-modal class="btn btn-primary mx-3">Nueva ruta de aprendizaje</button>
-        <dialog data-modal>
+        <dialog data-modal class="rounded rounded-md p-10">
             <p class="text-2xl mb-5">Nueva ruta de aprendizaje</p>
             <form action="<?=htmlspecialchars(base_url . "Carrera/add")?>" method="post" enctype="multipart/form-data">
                 <div class="mb-3">
@@ -14,16 +14,16 @@
                 </div>
                 <div class="mb-3"> 
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="img">Imagen del curso</label>
-                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="img_help" id="img" name="img" type="file" required>
-                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="img_help">PNG (MAX. 800x400px).</p>
+                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none" aria-describedby="img_help" id="img" name="img" type="file" required>
+                    <p class="mt-1 text-sm text-gray-500 " id="img_help">PNG (MAX. 800x400px).</p>
                 </div>
                 <div class="mb-3"> 
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="insignia">Insignia del curso</label>
-                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="insignia_help" id="insignia" name="insignia" type="file" required>
-                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="insignia_help">PNG (MAX. 800x400px).</p>
+                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none" aria-describedby="insignia_help" id="insignia" name="insignia" type="file" required>
+                    <p class="mt-1 text-sm text-gray-500  id="insignia_help">PNG (MAX. 800x400px).</p>
                 </div>
-                <div>
-                    <button data-close-modal class="btn btn-secundary">Cerrar</button>
+                <div  class="flex justify-center ">
+                    <button data-close-modal class="mx-5 btn btn-secundary">Cerrar</button>
                     <button type="submit" class="btn btn-primary">Agregar</button>
                 </div>
             </form>
